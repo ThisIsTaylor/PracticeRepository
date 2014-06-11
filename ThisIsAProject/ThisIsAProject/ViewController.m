457c73b0a156c96cc1d0c8b7640e336f90c043e8
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-06-11 16:41:21 -0400

    Tempest quote added

--- ThisIsAProject/ThisIsAProject/ViewController.m
+++ ThisIsAProject/ThisIsAProject/ViewController.m
@@ -37,19 +37,14 @@
 
 /*I am adding lots of comments down here so we can play around with it. Woohoo, here goes some more comments. In fact, let me share a monologue
  
- "If by your art, my dearest father, you have
- Put the wild waters in this roar, allay them.
- The sky, it seems, would pour down stinking pitch,
- But that the sea, mounting to the welkin's cheek,
- Dashes the fire out. O, I have suffered
- With those that I saw suffer: a brave vessel,
- Who had, no doubt, some noble creature in her,
- Dash'd all to pieces. O, the cry did knock
- Against my very heart. Poor souls, they perish'd.
- Had I been any god of power, I would
- Have sunk the sea within the earth or ere
- It should the good ship so have swallow'd and
- The fraughting souls within her."
+ "Not a soul
+ But felt a fever of the mad and play'd
+ Some tricks of desperation. All but mariners
+ Plunged in the foaming brine and quit the vessel,
+ Then all afire with me: the king's son, Ferdinand,
+ With hair up-staring,—then like reeds, not hair,—
+ Was the first man that leap'd; cried, 'Hell is empty
+ And all the devils are here." - Ariel
  
  */
 
