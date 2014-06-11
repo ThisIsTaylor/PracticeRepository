b84f6476a2cccc4d4c1268b460f3ceb860afdeb8
ThisIsTaylor <taylor.mcgregor@detroitlabs.com>
2014-06-11 16:22:02 -0400

    Initial Commit

---
+++ ThisIsAProject/ThisIsAProject/WAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WAAppDelegate.h
+//  ThisIsAProject
+//
+//  Created by DL Admin on 6/11/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ThisIsAProject/ThisIsAProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ThisIsAProject
+//
+//  Created by DL Admin on 6/11/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAAppDelegate class]));
+    }
+}
